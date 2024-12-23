d71c8db0e95024de9208ee78d51fb7b3b8492839
Stephani Bishop <ceo@apphouseinc.com>
2024-12-22 20:35:12 -0800

    mess up landing gear

--- src/components/Plane3D/parts/LandingGear.tsx
+++ src/components/Plane3D/parts/LandingGear.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { Cylinder, Box } from '@react-three/drei';
-import { COLORS } from '../../../utils/colors';
+import { Cylinder } from "@react-three/drei";
+import { COLORS } from "../../../utils/colors";
 
 export const LandingGear = () => {
   return (
     <group>
       {/* Main landing gear (left) */}
-      <group position={[0, -0.8, -0.3]}>
+      <group position={[0, -0.8, -1]}>
         <Cylinder args={[0.05, 0.05, 0.8]} position={[0, 0.4, 0]}>
           <meshStandardMaterial color={COLORS.STRUTS} />
         </Cylinder>
-            {/* tire */}
+      </group>
+
+      <group position={[0, -0.8, -1]}>
+        {/* tire */}
         <Cylinder args={[0.15, 0.15, 0.2]} rotation={[Math.PI / 2, 0, 0]}>
           <meshStandardMaterial color="#333333" />
         </Cylinder>
       </group>
 
       {/* Main landing gear (right) */}
-      <group position={[0, -0.8, 0.3]} >
-        <Cylinder args={[0.05, 0.05, 0.8]} position={[0, 0.4, 0]}>
-          <meshStandardMaterial color={COLORS.STRUTS} />
+      <group position={[0, -0.8, 1]}>
+        <Cylinder
+          args={[0.05, 0.05, 0.8]}
+          position={[0, 0.4, 0]}
+          rotation={[Math.PI / 1.5, 0, 0]}
+        >
+          <meshStandardMaterial color={"green"} />
         </Cylinder>
-        {/* tire */}
+      </group>
+
+      {/* tire */}
+      <group position={[0, -0.8, 1]}>
         <Cylinder args={[0.15, 0.15, 0.2]} rotation={[Math.PI / 2, 0, 0]}>
           <meshStandardMaterial color="#333333" />
         </Cylinder>
@@ -40,15 +49,13 @@ export const LandingGear = () => {
       {/* Wheel fairings */}
       <group>
         {/* Left fairing */}
-
-   
-        <mesh position={[0, -0.6, -0.6]} rotation={[1, 1.5, Math.PI * 1]}>
-          <boxGeometry args={[0.3, 0.4, 0.25]} />
+        <mesh position={[0, -0.7, -1]}>
+          <boxGeometry args={[0.3, 0.2, 0.25]} />
           <meshStandardMaterial color={COLORS.FUSELAGE} />
         </mesh>
         {/* Right fairing */}
-        <mesh position={[0, -0.6, 0.6]} rotation={[1, 1.5, Math.PI * 1]}>
-          <boxGeometry args={[0.3, 0.4, 0.25]} />
+        <mesh position={[0, -0.7, 1]}>
+          <boxGeometry args={[0.3, 0.2, 0.25]} />
           <meshStandardMaterial color={COLORS.FUSELAGE} />
         </mesh>
       </group>
